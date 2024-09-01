2c99d64c9104bab6f3dd0741fe611eb691f7a1f7
Ann <hells.pikachu@gmail.com>
2024-09-01 21:48:21 +0300

    add intro-80

---
+++ intro-80/main.js
@@ -0,0 +1,21 @@
+for (let i = 2; i <= 12; i = i + 2) {
+  console.log(i)
+}
+
+for (let p = 10; p >= 1; p-- ) {
+  console.log(p)
+}
+
+// ----------------------------
+
+let u = 0
+while (u <= 10) {
+  u = u + 2
+  console.log(u)
+}
+
+let o = 11
+while (o > 1) {
+  o = o - 1
+  console.log(o)
+}
