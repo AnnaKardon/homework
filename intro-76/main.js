f4a2c3cf43b2ee6252b44be8c4352ceeaa28176e
Ann <hells.pikachu@gmail.com>
2024-09-01 19:06:12 +0300

    add intro-76

---
+++ intro-76/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Ann'
+const lastName = 'Kardon'
+const gender = 'female'
+const age = '29'
+const feeling = 'tired'
+
+console.log(firstName, lastName, gender, age, 'y.o.', feeling)
