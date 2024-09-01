23d9e42d26738df081d3f08e814468fe1cdd33e5
Ann <hells.pikachu@gmail.com>
2024-09-01 21:48:42 +0300

    add intro-81

---
+++ intro-81/main.js
@@ -0,0 +1,12 @@
+const numbers = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+for (let i = 1; i < numbers.length; i++) {
+  console.log(numbers [i++])
+}
+
+// ------------------------------------------
+
+const colors = [`red`, `orange`, `yellow`, `green`, `blue`, `dark blue`, `purple`] 
+
+for (let p = colors.length - 1; p >= 0; p--) {
+  console.log(colors[p])
+}
